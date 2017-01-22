253a7154658c7be3d782e953aac6980670482ee6
Nikolay Moryakov <Nikolay Moryakov>
2017-01-22 17:22:46 +0300

    Add project files.

---
+++ Signature of File/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signature_of_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
